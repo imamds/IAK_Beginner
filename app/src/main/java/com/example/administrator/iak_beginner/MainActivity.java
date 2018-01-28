7399c665a3ace6f3e76f94f2c9a430c79fbda44a
imamds <imamds86@gmail.com>
2018-01-28 17:05:56 +0700

    add image LoginActivity

--- app/src/main/java/com/example/administrator/iak_beginner/MainActivity.java
+++ app/src/main/java/com/example/administrator/iak_beginner/MainActivity.java
@@ -4,9 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+
+    private final static String USERNAME = "username";
+    private final static String PASSWORD = "password";
+
+    private TextView tv_username;
+    private TextView tv_password;
+    private ImageView iv_gambar;
+
+    private String link_picture="https://i0.wp.com/www.amazine.co/wp-content/uploads/2013/12/Kucing_1.jpg?resize=650%2C487";
+
+
 
 
     @Override
@@ -15,6 +32,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: ");
+
+        tv_username = findViewById(R.id.tv_username);
+        tv_password = findViewById(R.id.tv_password);
+        iv_gambar = findViewById(R.id.iv_gambar);
+
+        Picasso.with(this)
+                .load(link_picture)
+                .into(iv_gambar, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(MainActivity.this, "Alhamdulillah, berhasil", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError() {
+                        Toast.makeText(MainActivity.this, "Astaghfirullah, gagal", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
 
     }
 
